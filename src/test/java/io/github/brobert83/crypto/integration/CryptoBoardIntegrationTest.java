c50e126bc69d6245d6c2f445b18b36bbf4832eb2
Robert Baboi <robert.baboi@gmail.com>
2020-09-30 13:24:57 +0100

    Setup maven and a dummy test to make sure it builds

---
+++ src/test/java/io/github/brobert83/crypto/integration/CryptoBoardIntegrationTest.java
@@ -0,0 +1,11 @@
+package io.github.brobert83.crypto.integration;
+
+import org.junit.Test;
+
+public class CryptoBoardIntegrationTest {
+
+    @Test
+    public void dummy(){
+        System.out.println("works");
+    }
+}
